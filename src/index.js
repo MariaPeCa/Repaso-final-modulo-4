27c17d389d5f78b443359b0148e00809bae85a88
MariaPeCa <mpenacantero@gmail.com>
2023-07-22 20:23:20 +0200

    endpoint post and put

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const server = express();
 // Configuración del servidor
 
 server.use(cors());
-server.use(express.json({limit: "25mb"}));
+server.use(express.json({ limit: "25mb" }));
 
 // Conexion a la base de datos
 
@@ -53,8 +53,8 @@ server.listen(port, () => {
 
 //consultar todos los gatos
 
-server.get('/api/kittens/:user', async (req, res)=> {
-   // recojo en una var la parte después del : y hago req.params y esa parte variable 
+server.get('/api/kittens/:user', async (req, res) => {
+  // recojo en una var la parte después del : y hago req.params y esa parte variable 
   const user = req.params.user;
   console.log(user);
   //sentencia sql para buscar los datos de un propietario en específico
@@ -65,7 +65,7 @@ server.get('/api/kittens/:user', async (req, res)=> {
   //result puede llamarse como yo quiera
   //la ejecución del select es asíncrono (hago un await)
   const [result] = await conn.query(select, user);
-  console.log(result); 
+  console.log(result);
   conn.end();
   res.json({
     info: {
@@ -75,4 +75,64 @@ server.get('/api/kittens/:user', async (req, res)=> {
     //results: kittenData --- listado de gatitos
     results: result, //mi listado de gatitos está en result
   });
+});
+//añadir un nuevo gato
+//vamos a recibir info por los dos parámetros
+server.post('/api/kittens/:user', async (req, res) => {
+  const user = req.params.user;
+  const newKitten = req.body;
+
+
+  try {
+    //pongo tantas interrogaciones como culumnas tengo, xq serán valores que aún no conozco porque son del nuevo gatito q se v a introducir
+    const insert = 'INSERT INTO kitten (`owner`, url, name, race, `desc`)VALUES (?,?,?,?,?)'
+    const conn = await getConnection();
+    //la query q hago ahora recibe varios parámetros, el primero es un insert y hago un destructuring de los otros dos parámetros de los que recibo info que son user y newKitten(las dos constantes que creé)
+    const [result] = await conn.query(insert, [
+      user,
+      newKitten.url,
+      newKitten.name,
+      newKitten.race,
+      newKitten.desc
+      //los datos anteriores me llegan del front, no tienen xq ser iguales q los de mi tabla pero es aconsejable q lo sean
+    ]);
+    conn.end();
+    res.json({
+      success: true,//puede ser true o false
+    });
+  } catch (error) {
+    res.json({
+      success: false, //puede ser true o false
+      //si me pidiesen devolver el error y así veo en postman q ha fallado
+      message: error,
+    });
+  }
+});
+
+//modificar un gatito ---> update sql
+//método PUT
+server.post('/api/kittens/:user/:kitten_id', async (req, res) => {
+  const user = req.params.user;
+  const kittenId = req.params.kitten_id;
+  const {urlFront, nameFront, raceFront, descFront } = req.body;
+  try {
+    const update = 'UPDATE kitten SET url= ?, name = ?, race= ?, `desc`= ? WHERE id = ?';
+    const conn = await getConnection();
+    const [result] = await conn.query(update, [
+      urlFront, 
+      nameFront, 
+      raceFront, 
+      descFront, 
+      kittenId,
+    ]);
+    conn.end();
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error
+    });
+  }
 });
